a0e7175092b51446c06cdc7b3f771f4636a8d309
Digdagdac <kimhs0459@naver.com>
2023-12-29 23:29:48 +0900

    스폰
    스폰

---
+++ Assets/Scripts/Herbs.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[System.Serializable]
+public class Herbs : MonoBehaviour
+{
+    
+    public GameObject herbsPrefab;
+    public float spawnProbability;
+    public float speed = 5f;
+
+    private Vector2 targetPosition;
+    public void SetTargetPosition(Vector2 target)
+    {
+        targetPosition = target;
+    }
+    void Update()
+    {
+     
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+
+       
+        transform.position = Vector2.MoveTowards(currentPosition, targetPosition, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Herbs[] herbs;
+    public Transform[] targetPoints;
+    public float SMINT = 2f;
+    public float SMAXT = 5f;
+    
+    public Transform spawnPoints;
+ 
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(2f, 5f));
+            SpawnHerbs();
+        }
+    }
+
+    void SpawnHerbs()
+    {
+        float totalProbability = 0;
+
+        
+        foreach (Herbs herb in herbs)
+        {
+            totalProbability += herb.spawnProbability;
+        }
+
+     
+        float randomValue = Random.Range(0, totalProbability);
+
+        
+        foreach (Herbs herb in herbs)
+        {
+            if (randomValue < herb.spawnProbability)
+            {
+                GameObject spawnedHerb = Instantiate(herb.herbsPrefab, spawnPoints.position, Quaternion.identity);
+
+                Transform targetPoint = targetPoints[Random.Range(0, targetPoints.Length)];
+
+              
+                Herbs Herbs = spawnedHerb.GetComponent<Herbs>();
+                if (Herbs != null)
+                {
+                    Herbs.SetTargetPosition(targetPoint.position);
+                }
+                return;
+            }
+            randomValue -= herb.spawnProbability;
+        }
+    }
+}
