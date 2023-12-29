7f188e1f513e3f4e8a728859e6827d34fe3a0426
Digdagdac <kimhs0459@naver.com>
2023-12-29 20:42:55 +0900

    add

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    public float dash = 5f;
+
+    private Vector2 dir = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+        if(dir != Vector2.zero)
+        {
+            rb.velocity = dir * speed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
